22acece890b2d8e6e3d0228bf8868a1e6a3cf8ab
Teemu Koivisto <tkoivisto456@gmail.com>
2020-03-04 13:53:21 +0200

    BE: fix parsing of query params

--- backend/src/middlewares/parseQueryParams.ts
+++ backend/src/middlewares/parseQueryParams.ts
@@ -1,6 +1,8 @@
 import { Response, NextFunction } from 'express'
 import * as Joi from 'joi'
 
+import { ValidationError } from '../common'
+
 import { AnyRequest } from '../types/general'
 
 export const parseQueryParams = (schema: Joi.ObjectSchema) => (req: AnyRequest, res: Response, next: NextFunction) => {
@@ -8,9 +10,11 @@ export const parseQueryParams = (schema: Joi.ObjectSchema) => (req: AnyRequest, 
 
   const result = Joi.validate(query, schema)
 
-  if (!result.error) {
+  if (result.error) {
+    next(new ValidationError(result.error.message))
+  } else {
     const mutatedReq = req as AnyRequest
     mutatedReq.queryParams = result.value
+    next()
   }
-  next()
 }
